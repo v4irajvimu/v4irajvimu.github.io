bb90fbcd9ad97ffddc91e5c6b8dd4fedecfc75e4
Vimukthi Jayasinghe <viraj.vimu@gmail.com>
2026-02-28 15:44:43 +0530

    chore: comment out ProjectsSection in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <AboutSection />
         <SkillsSection />
         <ExperienceSection />
-        <ProjectsSection />
+        {/* <ProjectsSection /> */}
         <BlogSection />
         <TestimonialsSection />
         <ContactSection />
